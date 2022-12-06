28a1df225c5dabe8cd120cfba1aa86a3f1c59dda
MyFreeR <my.free@mail.ru>
2022-12-06 12:08:46 +0300

    mns_board

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 class Board:
@@ -27,40 +28,50 @@ class Board:
 
     # cell - кортеж (x, y)
     def on_click(self, cell):
-        for i in range(self.width):
-            self.board[cell[1]][i] = (self.board[cell[1]][i] + 1) % 2
-        for i in range(self.height):
-            # чтобы не перекрашивать дважды
-            if i == cell[1]:
-                continue
-            self.board[i][cell[0]] = (self.board[i][cell[0]] + 1) % 2
+        # заглушка для реальных игровых полей
+        pass
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
         if cell:
             self.on_click(cell)
-        else:
-            print(cell)  # которая None
 
     def render(self, screen):
-        colors = [pygame.Color("black"), pygame.Color("white")]
         for y in range(self.height):
             for x in range(self.width):
-                pygame.draw.rect(screen, colors[self.board[y][x]],
+                pygame.draw.rect(screen, pygame.Color(255, 255, 255),
                                  (x * self.cell_size + self.left, y * self.cell_size + self.top,
-                                  self.cell_size, self.cell_size))  # заливка
-                pygame.draw.rect(screen, (100, 125, 250),
-                                 (self.left + x * self.cell_size, self.top + y * self.cell_size,
-                                 self.cell_size, self.cell_size), 2)
+                                  self.cell_size,
+                                  self.cell_size), 1)
+
+
+class Minesweeper(Board):
+    def __init__(self, width, height, n):
+        super().__init__(width, height)
+        # вначале все клетки закрыты
+        self.board = [[-1] * width for _ in range(height)]
+        i = 0
+        while i < n:
+            x = random.randint(0, self.width - 1)
+            y = random.randint(0, self.height - 1)
+            if self.board[y][x] == -1:
+                self.board[y][x] = 10
+                i += 1
 
 
 def main():
     pygame.init()
-    size = 500, 500
+    size = 320, 470
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Чёрное белое')
-    # поле 5 на 7
-    board = Board(5, 7)
+    clock = pygame.time.Clock()
+    pygame.display.set_caption('mnSweeperGrandpa')
+    board = Minesweeper(10, 15, 10)
+    board.set_view(10, 10, 30)
+
+    # Включено ли обновление поля
+    time_on = False
+    ticks = 0
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -68,9 +79,14 @@ def main():
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 board.get_click(event.pos)
+
         screen.fill((0, 0, 0))
         board.render(screen)
+
         pygame.display.flip()
+        clock.tick(50)
+        ticks += 1
+    pygame.quit()
 
 
 if __name__ == '__main__':
